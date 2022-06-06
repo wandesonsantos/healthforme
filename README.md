a8760e75b8deee6eedd1b3f6367228442b669c6e
wandeson <wandesonj29@gmail.com>
2022-06-06 01:02:27 -0300

    correçao

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Healthforme
 
+image.png
--- dashboard.php
+++ dashboard.php
@@ -24,10 +24,7 @@ include("auth_session.php");
       
 	<nav class="navbar navbar-expand-sm  navbar-dark">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>         
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          
             <div class="navbar-nav">
               <a class="nav-link active" aria-current="page" href="index.html">Home</a>
               <a class="nav-link" href="videos.html">Videos</a>
